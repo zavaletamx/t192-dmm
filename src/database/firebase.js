200add6d59e198b310dfa6b97455b52aec62e7dc
ZavaletaMX <raul.zavaletazea@gmail.com>
2021-02-18 10:58:46 -0600

    Creación de na base de datos en firebase con FireStore, intro a colecciones  y documentos, ejemplo para agregar docuemntos a una coleccion en FireStore desde react native expo

---
+++ src/database/firebase.js
@@ -0,0 +1,35 @@
+//Importamos todos los servicios de firebase
+/*
+1.- firestore
+2.- auth
+3.- storage
+4.- hosting
+*/
+import firebase from 'firebase';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyCWQqhdtFprhg_IZcTab6CtVJzOD2kdkO8',
+	authDomain: 'dmm-192.firebaseapp.com',
+	projectId: 'dmm-192',
+	storageBucket: 'dmm-192.appspot.com',
+	messagingSenderId: '24860642616',
+	appId: '1:24860642616:web:39bd50ba590990218005cd',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+/*
+Retornar los servicios de firebase 
+*/
+
+const db = firebase.firestore();
+const auth = firebase.auth();
+
+/* 
+Generamos una librería reutilizable
+*/
+export default {
+	db,
+	auth,
+};
--- src/screens/Inicio.js
+++ src/screens/Inicio.js
@@ -1,10 +1,38 @@
 import React from 'react';
-import { Button, Text, View } from 'react-native';
+import { Alert, Button, Text, View } from 'react-native';
+import firebase from './../database/firebase';
 
 /** props es una referencia a las variables, const, obj, componentes, etc
  * que comparte el componente padre conmigo
  */
 const Inicio = (props) => {
+	/*
+    Creamos una funcion flecha anonima que permita 
+    crear un documento usuario en la colección usuarios
+    */
+	const crearUsuarioFS = async () => {
+		try {
+			//Usamos el metodo asincrono colleccion.add
+			const usuario = {
+				nombre: 'Raul',
+				apellido: 'Zavaleta',
+			};
+
+			const usuarioFS = await firebase.db
+				.collection('usuarios')
+				.add(usuario);
+
+			Alert.alert(
+				'Practica 4',
+				`ID instertado:\n\n${usuarioFS.id}\n\nINSERTAR DESDE EL FORMULARIO DE REGISTRO`,
+				[{ text: 'Pues ya que', onPress: null }],
+				{ cancelable: false }
+			);
+		} catch (e) {
+			console.warn(e);
+		}
+	};
+
 	return (
 		<View
 			style={{
@@ -29,6 +57,11 @@ const Inicio = (props) => {
 				onPress={() => {
 					props.navigation.navigate('Registro');
 				}}
+			/>
+
+			<Button
+				title='Insertar en firestore'
+				onPress={crearUsuarioFS}
 			/>
 		</View>
 	);
